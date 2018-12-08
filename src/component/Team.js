f73229ad8e235b49b593b4589eb1e1a82bda30f4
rajesh <rajeshghosh4007@gmail.com>
2018-12-08 22:57:17 +0100

    context api data pass done

---
+++ src/component/Team.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Player from './Player'
+class Team extends Component {
+  render() {
+    return (
+      <Player/>
+    )
+  }
+}
+
+export default Team
