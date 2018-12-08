94e44d7893376d38e10836e2c05e0c56204cbedf
Pavel Samchuk <pavel.samchuk@gmail.com>
2018-12-08 23:58:18 +0100

    have a look

--- src/component/Player.js
+++ src/component/Player.js
@@ -1,44 +1,22 @@
-import React from 'react'
-import { MyContext } from './context'
-const  Player = ()=> {
-  
-  return (
-    <div className="container">
-          <MyContext.Consumer>
-              {(context) => 
-                  
-                {
-                  const {players} =context
-                  console.log(players)
-                
-                  return(
-                      players.map(player=>{
-                          return(
-                              <React.Fragment key={player.id}>
-                                  <div className="row" >
-                                      <div className="col s6 m6 offset-m3">
-                                          <div className="card blue lighten-2">
-                                              <span className="card-title">{player.name}</span>
-                                                <ul>
-                                                    <li>Age : {player.age}</li>
-                                                    <li>Position : {player.position}</li>
-                                                </ul>
-                                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto, cumque itaque! Optio dolore quae nesciunt velit dignissimos facilis placeat debitis inventore quis mollitia soluta, accusantium sapiente odit perferendis at ratione.</p>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </React.Fragment>
-                          )
-                      })
-                     
-                  )
-                }    
-              }
-          </MyContext.Consumer>
+import React from "react";
+const Player = ({ player }) => (
+  <div className="row">
+    <div className="col s6 m6 offset-m3">
+      <div className="card blue lighten-2">
+        <span className="card-title">{player.name}</span>
+        <ul>
+          <li>Age : {player.age}</li>
+          <li>Position : {player.position}</li>
+        </ul>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto,
+          cumque itaque! Optio dolore quae nesciunt velit dignissimos facilis
+          placeat debitis inventore quis mollitia soluta, accusantium sapiente
+          odit perferendis at ratione.
+        </p>
+      </div>
     </div>
-  )
-}
+  </div>
+);
 
-export default Player
-
-
+export default Player;
